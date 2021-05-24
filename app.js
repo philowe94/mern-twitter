35082498cbbc3b22caa14a1a5da8e15e080d8cc9
Philip Lowe <philip.lowe94@gmail.com>
2021-05-24 02:32:53 -0400

    project

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 //imports
 const mongoose = require("mongoose");
 const express = require("express");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+//import routes
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 //instantiate app
 const app = express();
 //import key for db
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 //connect to mongoDB using mongoose
 mongoose
   .connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to MongoDB successfully"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
-//route
-app.get("/", (req, res) => res.send("Hello Guyws"));
+//middleware setup
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(passport.initialize());
+require("./config/passport")(passport);
 
+//routes
+app.get("/", (req, res) => res.send("Hello Everybody"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 //assign port for server
 const port = process.env.PORT || 5000;
 
---
+++ frontend/src/reducers/root_reducer.js
@@ -0,0 +1,12 @@
+// src/reducers/root_reducer.js
+
+import { combineReducers } from 'redux';
+import errors from './errors_reducer';
+import session from './session_reducer';
+
+const RootReducer = combineReducers({
+  session,
+  errors
+});
+
+export default RootReducer;
